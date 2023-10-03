c8934804985e6e9ea98f8283597965644737ca6c
Pasindu Perera <pasinduperera9910@gmail.com>
2023-10-03 18:43:18 +0530

    commit

--- backend/router/users.router.js
+++ backend/router/users.router.js
@@ -9,6 +9,7 @@ const usersRouter = new Router(
 
 usersRouter.post('/new', async(ctx) =>
 {
+    // Simulating lack of input validation
     const user = await ctx.request.body;
     
     const newuser = await addUser(user);
@@ -29,22 +30,29 @@ usersRouter.post('/new', async(ctx) =>
 });
 
 usersRouter.get('/', async ctx=>{
+    // Simulating potential information disclosure
+    // Assuming no proper authorization checks are performed
     ctx.body= await getAllUsers();
 })
 
 usersRouter.delete('/:id', (ctx) => {
+    // Simulating potential insecure direct object reference (IDOR)
+    // Assuming no proper authorization checks are performed
     const id = ctx.params.id;
     ctx.body =  deleteUser(id);
     ctx.status = 204;
-
 });
 
 usersRouter.get('/get/:id', async ctx=> {
+    // Simulating potential insecure direct object reference (IDOR)
+    // Assuming no proper authorization checks are performed
     const id = ctx.params.id;
     ctx.body = await getUser(id);
 })
 
 usersRouter.put('/update/:id', async ctx=> {
+    // Simulating potential insecure direct object reference (IDOR)
+    // Assuming no proper authorization checks are performed
     const id = ctx.params.id;
     let user = ctx.request.body;
     user = await updateUser(id, user);
@@ -54,6 +62,7 @@ usersRouter.put('/update/:id', async ctx=> {
 
 usersRouter.post('/login', async(ctx) =>
 {
+    // Simulating lack of secure authentication
     const user = await ctx.request.body;
     
     const isuser = await loginuser(user);
